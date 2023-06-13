4c3157bfb3cab472cc15bd4878e8f31e110b9121
hyuqi <heyuq23@163.com>
2023-06-13 11:51:50 +0800

    GitServlet重写方法

--- git_demo/git_demo/src/main/java/com/example/git_demo/GitServlet.java
+++ git_demo/git_demo/src/main/java/com/example/git_demo/GitServlet.java
@@ -1,4 +1,21 @@
 package com.example.git_demo;
 
-public class GitServlet {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+// 蓝色 表示改内容提交到了本地仓库之后  又进行了修改
+public class GitServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
 }
